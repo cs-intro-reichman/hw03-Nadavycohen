c41356e8989d7c59f7019f18ab3f195c34e83e6c
Nadavycohen <128643278+Nadavycohen@users.noreply.github.com>
2024-01-11 08:45:57 -0500

    UniqueChars.java

--- UniqueChars.java
+++ UniqueChars.java
@@ -1,29 +1,24 @@
-
 public class UniqueChars {
     public static void main(String[] args) {  
-        String str = args[0];
-        System.out.println(uniqueChars(str));
-    }
-
-    public static String uniqueChars(String str) {
-        String uniq = "";
-		for (int i = 0; i < str.length(); i++) {
-			char letter = str.charAt(i);
-			boolean duplicate = false;
-			for (int j = 0; j < uniq.length(); j++) {
-				if (uniq.charAt(j) == letter) {
-					duplicate = true;
-					break;
-				}		
-			}
-			if (!duplicate || letter == ' ') {
-				uniq += letter;
-
-			}
-
-		}
-
-		return uniq;
-	}
-
-}
+         String str = args[0];
+         System.out.println(uniqueChars(str));
+     }
+ 
+     public static String uniqueChars(String s) 
+     {
+        String uniq= "";
+        
+        for (int i = 0; i<s.length(); i++)
+        {
+         char letter = s.charAt(i);
+          if (uniq.indexOf(letter) == -1)
+          {
+           uniq+=letter;
+            }  else if(letter == ' ') 
+            {
+             uniq +=letter;
+            }  
+        }
+         return uniq;
+     }
+ }
