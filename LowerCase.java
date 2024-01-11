25608b0c703e17f054b852f70e882d83ff8d73f3
Nadavycohen <128643278+Nadavycohen@users.noreply.github.com>
2024-01-11 08:34:08 -0500

    LowerCase.java

--- LowerCase.java
+++ LowerCase.java
@@ -1,17 +1,19 @@
 public class LowerCase {
     public static void main(String[] args) {  
         String str = args[0];
-        System.out.println(lowerCase(str));}
+        System.out.println(lowerCase(str));
+    }
 
-    public static String lowerCase(String str) {
+
+    public static String lowerCase(String s) {
         String low = "";
-		for (int i = 0; i < str.length(); i++) {
-			char ch = str.charAt(i);
-			if ((ch >= 'A') && (ch <= 'Z')){
-				low = low + (char)(str.charAt(i) + 32);
+		for (int i = 0; i < s.length(); i++) {
+			char letter = s.charAt(i);
+			if ((letter >= 'A') && (letter <= 'Z')){
+				low = low + (char)(s.charAt(i) + 32);
 			} else 
             { 
-				low = low + str.charAt(i);
+				low = low + s.charAt(i);
 			}
 		}
 
